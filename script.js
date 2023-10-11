21fc974ae0a43fab87e93adb27736dd8a7b2de87
JohnRehagen <136288910+JohnRehagen@users.noreply.github.com>
2023-10-10 22:44:45 -0500

    Mobile + Not
    Ok this one actually works. The website reload function (in JS inside of HTML file) does not work right now and the debounce is a major problem for mobile. I would like to solve it and add it back but will need more work. Commented out for now

--- script.js
+++ script.js
@@ -1,212 +1,154 @@
-window.addEventListener('load', function () {
-    // Get the height of the sidebar
-    const sidebar = document.querySelector('.sidebar');
-    const sidebarHeight = sidebar.offsetHeight ;
+var offset = 0;
 
-    // Set the margin-top of the content to the height of the sidebar
-    const content = document.querySelector('.content');
-    if (window.innerWidth <= 768) {
-        content.style.marginTop = sidebarHeight + 'px';
-    }
-});
-
-
-if (window.innerWidth > 768){
-    
+// Function to execute code for small screens
+function executeCodeForSmallScreens() {
     document.addEventListener("DOMContentLoaded", () => {
-        const sections = document.querySelectorAll(".section");
-        const sidebarLinks = document.querySelectorAll(".sections a");
-        const cursorHighlight = document.createElement("div");
-        cursorHighlight.className = "cursor-highlight";
-        document.body.appendChild(cursorHighlight);
-
-        const viewportHeight = window.innerHeight;
-        const offsetPercentage = 20;
-        const offset = (viewportHeight * offsetPercentage) / 100;
-
-        /*
-        if (window.innerWidth > 768) {
-            const viewportHeight = window.innerHeight;
-            const offsetPercentage = 20;
-            const offset = (viewportHeight * offsetPercentage) / 100;
-        }
-
-        if (window.innerWidth <= 768) {
-            const sidebar = document.querySelector('.sidebar');
-            const offset = sidebar.offsetHeight * 1.1;
-        }
-        */
-
-        // Function to highlight the active section in the sidebar and change text color
-        function highlightActiveSection() {
-            const scrollPosition = window.scrollY;
-
-            // Determine which section is currently in view
-            let activeSection = null;
-
-            sections.forEach((section) => {
-                const sectionTop = section.offsetTop - offset;
-                const sectionBottom = sectionTop + section.offsetHeight;
-
-                if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-                    activeSection = section;
-                }
-            });
-
-            // Remove any existing highlighting
-            sidebarLinks.forEach((link) => {
-                link.classList.remove("active");
-            });
-
-            // Highlight the active section in the sidebar and change text color
-            if (activeSection) {
-                const activeLinkId = activeSection.getAttribute("id");
-                sidebarLinks.forEach((link) => {
-                    if (link.getAttribute("href").substring(1) === activeLinkId) {
-                        link.classList.add("active");
-                    }
-                });
-            }
-        }
-
-        // Function to update cursor highlight
-        function updateCursorBackground(e) {
-            const mouseX = e.clientX;
-            const mouseY = e.clientY;
-            cursorHighlight.style.left = `${mouseX}px`;
-            cursorHighlight.style.top = `${mouseY}px`;
-        }
-
-        // Function to handle scroll events and prevent scrolling of the cursor highlight
-        function handleScroll(e) {
-            e.preventDefault();
-            const mouseX = e.clientX;
-            const mouseY = e.clientY;
-            cursorHighlight.style.left = `${mouseX}px`;
-            cursorHighlight.style.top = `${mouseY}px`;
-        }
-
-        // Listen for scroll events to highlight the active section
-        window.addEventListener("scroll", () => {
-            highlightActiveSection();
-        });
-
-        // Add event listener for mousemove to update cursor position
-        document.addEventListener("mousemove", updateCursorBackground);
-
-        // Add event listener for scroll to prevent scrolling of cursor highlight
-        document.addEventListener("scroll", handleScroll);
-
-        // Initial highlight when the page loads
-        highlightActiveSection();
-
-        // Add smooth scrolling behavior to section links
-        // Modify the JavaScript code to exclude links with a specific class
-        const sectionLinks = document.querySelectorAll(".sections a:not(.external-link)");
-
-        sectionLinks.forEach((link) => {
-            link.addEventListener("click", (e) => {
-                e.preventDefault();
-                const targetId = link.getAttribute("href").substring(1);
-                const targetSection = document.getElementById(targetId);
-                if (targetSection) {
-                    const targetOffset = targetSection.offsetTop - (viewportHeight * 0.19);
-                    window.scrollTo({ top: targetOffset, behavior: "smooth" });
-                }
-            });
-        });
-    });
-}
-
-if (window.innerWidth <= 768){
-    
-    document.addEventListener("DOMContentLoaded", () => {
-        const sections = document.querySelectorAll(".section");
-        const sidebarLinks = document.querySelectorAll(".sections a");
-        const cursorHighlight = document.createElement("div");
-        cursorHighlight.className = "cursor-highlight";
-        document.body.appendChild(cursorHighlight);
-
+        console.log("Executing code for small screens");
+        // Get the height of the sidebar
         const sidebar = document.querySelector('.sidebar');
-        const offset = sidebar.offsetHeight+50;
+        const sidebarHeight = sidebar.offsetHeight + 50;
+        offset = sidebar.offsetHeight + 80 ;
+        console.log("Offset for small screens:", offset);
 
-        // Function to highlight the active section in the sidebar and change text color
-        function highlightActiveSection() {
-            const scrollPosition = window.scrollY;
+        // Set the margin-top of the content to the height of the sidebar
+        const content = document.querySelector('.content');
+        content.style.marginTop = sidebarHeight + 'px';
 
-            // Determine which section is currently in view
-            let activeSection = null;
-
-            sections.forEach((section) => {
-                const sectionTop = section.offsetTop - offset;
-                const sectionBottom = sectionTop + section.offsetHeight;
-
-                if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-                    activeSection = section;
-                }
-            });
-
-            // Remove any existing highlighting
-            sidebarLinks.forEach((link) => {
-                link.classList.remove("active");
-            });
-
-            // Highlight the active section in the sidebar and change text color
-            if (activeSection) {
-                const activeLinkId = activeSection.getAttribute("id");
-                sidebarLinks.forEach((link) => {
-                    if (link.getAttribute("href").substring(1) === activeLinkId) {
-                        link.classList.add("active");
-                    }
-                });
-            }
-        }
-
-        // Function to update cursor highlight
-        function updateCursorBackground(e) {
-            const mouseX = e.clientX;
-            const mouseY = e.clientY;
-            cursorHighlight.style.left = `${mouseX}px`;
-            cursorHighlight.style.top = `${mouseY}px`;
-        }
-
-        // Function to handle scroll events and prevent scrolling of the cursor highlight
-        function handleScroll(e) {
-            e.preventDefault();
-            const mouseX = e.clientX;
-            const mouseY = e.clientY;
-            cursorHighlight.style.left = `${mouseX}px`;
-            cursorHighlight.style.top = `${mouseY}px`;
-        }
-
-        // Listen for scroll events to highlight the active section
-        window.addEventListener("scroll", () => {
-            highlightActiveSection();
-        });
-
-        // Add event listener for mousemove to update cursor position
-        document.addEventListener("mousemove", updateCursorBackground);
-
-        // Add event listener for scroll to prevent scrolling of cursor highlight
-        document.addEventListener("scroll", handleScroll);
-
-        // Initial highlight when the page loads
-        highlightActiveSection();
-
-        // Add smooth scrolling behavior to section links
-        // Modify the JavaScript code to exclude links with a specific class
-        const sectionLinks = document.querySelectorAll(".sections a:not(.external-link)");
-
-        sectionLinks.forEach((link) => {
-            link.addEventListener("click", (e) => {
-                e.preventDefault();
-                const targetId = link.getAttribute("href").substring(1);
-                const targetSection = document.getElementById(targetId);
-                if (targetSection) {
-                    const targetOffset = targetSection.offsetTop - (viewportHeight * 0.19);
-                    window.scrollTo({ top: targetOffset, behavior: "smooth" });
-                }
-            });
+        // Select all elements with a specific class
+        const elementsToHide = document.querySelectorAll('.Socials-section');
+    
+        // Hide those elements by setting their display property to 'none'
+        elementsToHide.forEach((element) => {
+            element.style.display = 'none';
         });
     });
 }
+
+// Function to execute code for larger screens
+function executeCodeForLargerScreens() {
+    const viewportHeight = window.innerHeight;
+    const offsetPercentage = 20;
+    offset = (viewportHeight * offsetPercentage) / 100;
+    console.log("Offset for larger screens:", offset);
+}
+
+let oldWindowWidth = window.innerWidth;
+
+// Function to execute code for lateral resizing
+function executeCodeOnLateralResize() {
+    const newWindowWidth = window.innerWidth;
+
+    if (newWindowWidth !== oldWindowWidth) {
+        console.log("Executing code for lateral resizing");
+        // Execute your code for screen width changes
+        if (newWindowWidth <= 768) {
+            executeCodeForSmallScreens();
+        } else {
+            executeCodeForLargerScreens();
+        }
+
+        // Update the old window width
+        oldWindowWidth = newWindowWidth;
+    }
+}
+
+function initializeScreenSize(){
+    if (window.innerWidth <= 768) {
+        executeCodeForSmallScreens();
+    } else {
+        executeCodeForLargerScreens();
+    }
+}
+
+initializeScreenSize()
+
+// Add an event listener for window resize
+window.addEventListener("resize", executeCodeOnLateralResize);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".section");
+    const sidebarLinks = document.querySelectorAll(".sections a");
+    const cursorHighlight = document.createElement("div");
+    cursorHighlight.className = "cursor-highlight";
+    document.body.appendChild(cursorHighlight);
+
+    // Function to highlight the active section in the sidebar and change text color
+    function highlightActiveSection() {
+        const scrollPosition = window.scrollY;
+
+        // Determine which section is currently in view
+        let activeSection = null;
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop - offset;
+            const sectionBottom = sectionTop + section.offsetHeight;
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSection = section;
+            }
+        });
+
+        // Remove any existing highlighting
+        sidebarLinks.forEach((link) => {
+            link.classList.remove("active");
+        });
+
+        // Highlight the active section in the sidebar and change text color
+        if (activeSection) {
+            const activeLinkId = activeSection.getAttribute("id");
+            sidebarLinks.forEach((link) => {
+                if (link.getAttribute("href").substring(1) === activeLinkId) {
+                    link.classList.add("active");
+                }
+            });
+        }
+    }
+
+    // Function to update cursor highlight
+    function updateCursorBackground(e) {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        cursorHighlight.style.left = `${mouseX}px`;
+        cursorHighlight.style.top = `${mouseY}px`;
+    }
+
+    // Function to handle scroll events and prevent scrolling of the cursor highlight
+    function handleScroll(e) {
+        e.preventDefault();
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        cursorHighlight.style.left = `${mouseX}px`;
+        cursorHighlight.style.top = `${mouseY}px`;
+    }
+
+    // Listen for scroll events to highlight the active section
+    window.addEventListener("scroll", () => {
+        highlightActiveSection();
+    });
+
+    // Add event listener for mousemove to update cursor position
+    document.addEventListener("mousemove", updateCursorBackground);
+
+    // Add event listener for scroll to prevent scrolling of cursor highlight
+    document.addEventListener("scroll", handleScroll);
+
+    // Initial highlight when the page loads
+    highlightActiveSection();
+
+    // Add smooth scrolling behavior to section links
+    // Modify the JavaScript code to exclude links with a specific class
+    const sectionLinks = document.querySelectorAll(".sections a:not(.external-link)");
+
+    sectionLinks.forEach((link) => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+            if (targetSection) {
+                const viewportHeight = window.innerHeight;
+                const targetOffset = targetSection.offsetTop - (viewportHeight * 0.19);
+                window.scrollTo({ top: targetOffset, behavior: "smooth" });
+            }
+        });
+    });
+});
